c4464c56186a345ed5baea60a50961105ccd3daa
DeLittleCat <113717648+DeLittleCat@users.noreply.github.com>
2024-08-12 17:06:57 -0500

    Created graveler.py
    Uses multithreading to make and compare and generate multiple random numbers at a time. This is my first time using multithreading. I also compressed it as much as I knew how.

---
+++ graveler.py
@@ -0,0 +1,16 @@
+from time import time
+import numpy as np
+import numba as nb
+
+
+@nb.njit(nb.void(nb.u4), parallel=True)
+def main(executions):
+    threadBests = np.ones(nb.get_num_threads(), dtype=np.uint8)
+    for _ in nb.prange(executions):
+        ones, thread = np.sum(np.random.randint(1, 5, size=231) == 1), nb.get_thread_id()
+        if ones > threadBests[thread]: threadBests[thread] = ones
+    best = np.max(threadBests)
+    print("Most Ones Rolled:", best if best < 177 else 177, "\nNumber of Executions:", executions)
+
+
+if __name__ == "__main__": startTime=time(); main(1000); print("Done in:",time()-startTime,"s")
