ebc2375f0ffbcbeba298255a86fa47c072dc2d55
DeLittleCat <113717648+DeLittleCat@users.noreply.github.com>
2024-09-13 16:00:17 -0500

    Update graveler.py
    Replaced timing method
    Added documentation

--- graveler.py
+++ graveler.py
@@ -1,14 +1,20 @@
-from time import time
+from timeit import timeit
 import numpy as np
 import numba as nb
 
 
 @nb.njit(nb.void(nb.u4), parallel=True)
-def main(executions):
-    threadBests = np.ones(nb.get_num_threads(), dtype=np.uint8)
+def main(executions: int) -> None:
+    """A function that generates an arbitrary
+       amount of random binomials and prints
+       the largest or 177, whichever is
+       smaller, alongside the arbitrary amount."""
+    
+    threadBests = np.zeros(nb.get_num_threads(), dtype=np.uint8)
     for _ in nb.prange(executions):
-        ones, thread = np.random.binomial(n=231, p=0.25), nb.get_thread_id()
-        if ones > threadBests[thread]: threadBests[thread] = ones
+        paras, thread = np.random.binomial(n=231, p=0.25), nb.get_thread_id()
+        if paras > threadBests[thread]: threadBests[thread] = paras
     print("Most Ones Rolled:", min(np.max(threadBests), 177), "\nNumber of Executions:", executions)
 
-if __name__ == "__main__": startTime = time(); main(1E9); print("Done in:", time() - startTime, "s")
+
+if __name__ == "__main__": print("Done in:", timeit(globals=globals(), stmt="main(1E9)", number=1), "s")
