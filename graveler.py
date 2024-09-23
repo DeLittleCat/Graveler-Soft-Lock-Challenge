eee4f6568c99ab9ede7f50c6c7ec5c13c24b86fe
DeLittleCat <113717648+DeLittleCat@users.noreply.github.com>
2024-09-23 13:59:59 -0500

    Update graveler.py
    Renamed a variable
    Retimed at 125.62554710009135 s, for 4x 1.1 GHz

--- graveler.py
+++ graveler.py
@@ -10,11 +10,11 @@ def main(executions: int) -> None:
        the largest or 177, whichever is
        smaller, alongside the arbitrary amount."""
     
-    threadBests = np.zeros(nb.get_num_threads(), dtype=np.uint8)
+    threadMaxes = np.zeros(nb.get_num_threads(), dtype=np.uint8)
     for _ in nb.prange(executions):
         paras, thread = np.random.binomial(n=231, p=0.25), nb.get_thread_id()
-        if paras > threadBests[thread]: threadBests[thread] = paras
-    print("Most Ones Rolled:", min(np.max(threadBests), 177), "\nNumber of Executions:", executions)
-
+        if paras > threadMaxes[thread]: threadMaxes[thread] = paras
+    print("Most Paralyzations:", min(np.max(threadMaxes), 177), "\nNumber of Executions:", executions)
+    
 
 if __name__ == "__main__": print("Done in:", timeit(globals=globals(), stmt="main(1E9)", number=1), "s")
