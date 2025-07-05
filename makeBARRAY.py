80e0214557b38301d6dbf481054499858ff1f9eb
DeLittleCat <113717648+DeLittleCat@users.noreply.github.com>
2025-07-05 04:24:32 -0500

    Update makeBARRAY.py
    Changed binomial coefficient algorithm
    Improved readability significantly

--- makeBARRAY.py
+++ makeBARRAY.py
@@ -1,40 +1,57 @@
+from decimal import Decimal, getcontext
+from time import time
 import numpy as np
 import os
-from decimal import Decimal, getcontext
-from timeit import timeit
 
 
-#Increase accuracy ▼                       ▼ Print out certain containers/values
-def main(prec: int=2**20, printExact: bool=False) -> None:
-    def decList(length: int, fract: tuple) -> list:
-        def facList(length: int, p: int=1) -> list:
-            return [1] + [p := p * i for i in range(1, length)]
+def findZeroAndOne(arr: np.array) -> np.array:
+    zo = np.zeros(2, dtype=np.uint8)
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            zo[0] += 1
+        elif arr[i] == 1:
+            zo[1] += 1
+    return zo
 
-        def cumsum(lis: list, s: int=0) -> list: # For Decimal objects
-            return [s := s + x for x in lis]
+def printApprox(arr: np.array, arrName: str) -> None:
+    for i in range(len(arr)):
+        print(f"{arrName}[{i}]: ~{arr[i]:.{1024}f}".rstrip("0").rstrip("."))
+    print("\n")
+
     
-        qn, fac, length = fract[1] - fract[0], facList(length), length - 1
-        dlf, pdPowLen = Decimal(fac[-1]), fract[1]**length
-        return cumsum([qn**i * dlf / (pdPowLen * fac[i] * fac[length - i]) for i in range(length, -1, -1)])
-    def fixcd(cdArray: list) -> list:
-        return np.unique(cdArray[(cdArray > 0)])
-    getcontext().prec = prec
-    decLis = decList(232, (1,4))
-    floArrMTB, floArrPTB = np.array([float(x) for x in decLis], dtype=np.float64), np.array([float(x**1000000000) for x in decLis], dtype=np.float64)
-    cdArrMTB, cdArrPTB = fixcd(floArrMTB), fixcd(floArrPTB)
-    np.save(os.path.join(os.path.dirname(__file__), "cdArrayMTB.npy"), cdArrMTB)
-    np.save(os.path.join(os.path.dirname(__file__), "cdArrayPTB.npy"), cdArrPTB)
-    if printExact:
-        print("Exact Decimals:")
-        for x in decLis: print(f"{x:.{prec}f}".rstrip("0").rstrip("."))
-        print("Exact Floats(for MTB):")
-        for x in floArrMTB: print(f"{x:.{1074}f}".rstrip("0").rstrip("."))
-        print("Exact Floats(for PTB):")
-        for x in floArrPTB: print(f"{x:.{1074}f}".rstrip("0").rstrip("."))
-        print("End results:\n   MTB:", cdArrMTB, "\n   PTB:", cdArrPTB)
-    print("For PTB, change the added number to:", np.nonzero(floArrPTB)[0][0])
+st = time()
+getcontext().prec = 2**24
+PRINT = True
+p = Decimal(1)/4
+q = 1 - p
+nCk = Decimal(1)
+sum = q**231
 
+cdf = np.zeros(232, dtype=Decimal)
+cdf[0] = sum
+for i in range(1, 232):
+    nCk = nCk * (232 - i) // i
+    sum += nCk * p**i * q**(231 - i)
+    cdf[i] = sum
+pcdf = [x**1000000000 for x in cdf]
 
-if __name__ == "__main__": print("Done in:", timeit(globals=globals(), stmt="main()", number=1), "s")
-    
+if PRINT:
+    print("Decimal Values: ")
+    printApprox(cdf, "cdf")
+    printApprox(pcdf, "pcdf")
 
+cdf = [float(x) for x in cdf]
+pcdf = [float(x) for x in pcdf]
+cdfzo = findZeroAndOne(cdf)
+pcdfzo = findZeroAndOne(pcdf)
+cdf = cdf[cdfzo[0]:232 - cdfzo[1]]
+pcdf = pcdf[pcdfzo[0]:232 - pcdfzo[1]]      
+
+if PRINT:
+    print("Stored Values: ")
+    printApprox(cdf, "cdf")
+    printApprox(pcdf, "pcdf")
+
+np.save(os.path.join(os.path.dirname(__file__), "cdArrayMTB.npy"), cdf)
+np.save(os.path.join(os.path.dirname(__file__), "cdArrayPTB.npy"), pcdf)
+print("Change the offset of PTB(after np.searchsorted in main) to", pcdfzo[0], "if it's not already.\nDone in:", time() - st, "s")
